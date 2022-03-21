a74863d8e6aa9dd6f74cf95e985d1ba36f0039de
alban-okoby <albanokoby225@gmail.com>
2022-03-21 01:10:05 +0000

    Stylisation avec interaction de navbar et section

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,28 @@ $(document).ready(function(){
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-toggle');
     });
+
+    
+  $(window).on('scroll load',function(){
+
+    $('.fa-bars').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+
+    if($(window).scrollTop() > 30){
+      $('header').addClass('header-active');
+    }else{
+      $('header').removeClass('header-active');
+    }
+
+    $('section').each(function(){
+        let id = $(this).attr('id');
+        let height = $(this).height();
+        let offset = $(this).offset().top - 200;
+        let top = $(window).scrollTop();
+        if(top >= offset && top < offset + height){
+          $('.navbar ul li a').removeClass('active');
+          $('.navbar').find('[href="#' + id + '"]').addClass('active');
+        }
+    })
+});
 });
