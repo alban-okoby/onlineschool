e39925eb12ce824f9fe82803580628c0ea5534f6
alban-okoby <albanokoby225@gmail.com>
2022-06-04 14:38:30 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function () {
+	// Licence | DigitalSouag by Alban Okoby
 
-    $('.fa-bars').click(function(){
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
+	// Menu navbar icon when user click
+	$('.fa-bars').click(function () {
+		$(this).toggleClass('fa-times');
+		$('.navbar').toggleClass('nav-toggle');
+	});
 
-    
-  $(window).on('scroll load',function(){
+	// Remove menu icon when user scroll on page
+	$(window).on('scroll load', function () {
 
-    $('.fa-bars').removeClass('fa-times');
-    $('.navbar').removeClass('nav-toggle');
-
-    if($(window).scrollTop() > 60){
-      $('header').addClass('header-active');
-      if($(window).scrollTop() > 320){
-        $('header').addClass('header-active-design');
-      }
-    }else{
-      $('header').removeClass('header-active');
-      $('header').removeClass('header-active-design');
-    }
-
-    $('section').each(function(){
-        let id = $(this).attr('id');
-        let height = $(this).height();
-        let offset = $(this).offset().top - 200;
-        let top = $(window).scrollTop();
-        if(top >= offset && top < offset + height){
-          $('.navbar ul li a').removeClass('active');
-          $('.navbar').find('[href="#' + id + '"]').addClass('active');
-        }
-    })
-});
+		$('.fa-bars').removeClass('fa-times');
+		$('.navbar').removeClass('nav-toggle');
+	// Fixed navbar on top when user scrool > 60px
+		if ($(window).scrollTop() > 60) {
+			$('header').addClass('header-active');
+			if ($(window).scrollTop() > 320) {
+				$('header').addClass('header-active-design');
+			}
+		} else {
+			$('header').removeClass('header-active');
+			$('header').removeClass('header-active-design');
+		}
+		// Indicate navbar link navigation
+		$('section').each(function () {
+			let id = $(this).attr('id');
+			let height = $(this).height();
+			let offset = $(this).offset().top - 200;
+			let top = $(window).scrollTop();
+			if (top >= offset && top < offset + height) {
+				$('.navbar ul li a').removeClass('active');
+				$('.navbar').find('[href="#' + id + '"]').addClass('active');
+			}
+		})
+	});
 });
